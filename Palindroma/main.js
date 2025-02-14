77484be1ba9622b97325d8119b103c925fa88d54
tommaso.dare <tommaso.dare96@gmail.com>
2025-02-14 16:51:23 +0100

    Done Palindroma Exercise

--- Palindroma/main.js
+++ Palindroma/main.js
@@ -1,2 +1,16 @@
 /* Palidroma
 Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma */
+
+const userWord = prompt("Inserisci una parola")
+
+console.log(userWord);
+
+const userWordReverse = userWord.reverseString()
+
+console.log(userWordReverse);
+
+if (userWordReverse === userWord) {
+    console.log("La parola è palindroma");
+} else {
+    console.log("La parola non è palindroma");
+}
